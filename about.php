fab9219035b67e37ef10a1af2c97a26a6b815af8
Amasi-G <111519508+Amasi-G@users.noreply.github.com>
2022-09-06 21:35:28 +0530

    changes header content
    abcec

---
+++ about.php
@@ -0,0 +1,101 @@
+<!--
+author: Boostraptheme
+author URL: https://boostraptheme.com
+License: Creative Commons Attribution 4.0 Unported
+License URL: https://creativecommons.org/licenses/by/4.0/
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <?php include("basic_info.php"); ?>
+	<!-- Global Stylesheets -->
+    <?php  include("style_global.php"); ?>
+	<!-- Core Stylesheets -->
+    <link rel="stylesheet" href="css/about.css">
+</head>
+
+<body id="page-top">
+
+<!--====================================================
+                         HEADER
+======================================================-->
+
+    <header>
+		<?php include("header.php"); ?>
+    </header> 
+
+<!--====================================================
+                    LOGIN OR REGISTER
+======================================================-->
+    <section id="login">
+      <?php include("login.php"); ?>
+    </section>      
+
+<!--====================================================
+                       HOME-P
+======================================================-->
+    <div id="home-p" class="home-p pages-head1 text-center">
+        <div class="container">
+               </div>
+        <!--/end container-->
+    </div>
+
+<!--====================================================
+                        ABOUT-P1
+======================================================-->
+    <section id="about-p1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="about-p1-cont">
+                        <h1>Who we are</h1>
+                        <p> Aptabs is a pharmaceutical company that specialises in innovative and patient-focused treatments for severa infections and viral fevers.</p>
+<p>We are a leader in the treatment of different types of lung allergy and cough where our products treat over good number of patients annually.
+Located in Guntur , we are a mid-size pharmaceutical company actively engaged with patients advocates, healthcare professionals and policy-makers.
+</p>
+                       
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="about-p1-img">
+                        <img src="img/ipad-tablet.png" class="img-fluid wow fadeInUp" data-wow-delay="0.1s" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!--====================================================
+                       ABOUT-P2
+======================================================-->
+    
+
+<!--====================================================
+                     ABOUT-P3
+======================================================-->
+    
+
+<!--====================================================
+                    CONTACT HOME
+======================================================-->
+    
+
+<!--====================================================
+                       NEWS
+======================================================-->
+   
+
+<!--====================================================
+                      FOOTER
+======================================================-->
+    <footer>
+       <?php include("footer.php"); ?>
+    </footer>
+    <!--Global JavaScript -->
+    <?php  include("script_global.php"); ?>
+</body>
+
+</html>
